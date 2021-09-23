072a70238ef86f64a133768b64026a52deb53457
Gerry Babuin <gerrybabuin@gmail.com>
2021-09-23 11:04:24 -0600

    Fix bugs from rebase

--- server/handlers.js
+++ server/handlers.js
@@ -29,7 +29,6 @@ const getSingleProduct = (req, res) => {
   }
 };
 
-const { getAllCompanies } = require("./data");
 const {
   getAllCompanies,
   getAllUsers,
@@ -356,11 +355,7 @@ const handlePurchase_t = (req, res) => {
   }
 };
 
-
-const { getAllProducts } = require("./data");
-
 const handleProduct = (req, res) => {
-
   const first20 = getAllProducts().slice(0, 20);
   res.status(200).json({
     status: 200,
@@ -370,58 +365,57 @@ const handleProduct = (req, res) => {
 };
 
 const handleSingleProduct = (req, res) => {
-    const { itemId } = req.params;
-    const singleItem = items.filter((item) => {
-      return item._id === Number(`${req.params.itemId}`);
-    });
+  const { itemId } = req.params;
+  const singleItem = items.filter((item) => {
+    return item._id === Number(`${req.params.itemId}`);
+  });
 
-    if (singleItem.length > 0) {
-      res.status(200).json({
-        status: 200,
-        message: `Successfully retrieved item ${itemId} `,
-        item: singleItem,
-      });
-    } else {
-      res.status(404).json({
-        status: 404,
-        message: "Couldn't find item",
-      });
-    }
+  if (singleItem.length > 0) {
+    res.status(200).json({
+      status: 200,
+      message: `Successfully retrieved item ${itemId} `,
+      item: singleItem,
+    });
+  } else {
+    res.status(404).json({
+      status: 404,
+      message: "Couldn't find item",
+    });
   }
+};
 
-  const sortCategory = (req) => {
-    const { category } = req.params;
-  
-    // filters the items by category into a new array
-    let filteredItems = items.filter((item) => {
-      if (item.category === category) {
-        return item;
-      }
-    });
-  
-    return filteredItems;
-  };
+const sortCategory = (req) => {
+  const { category } = req.params;
 
-  module.exports = {
-    handleProduct,
-    handleSingleProduct,
-    sortCategory,
-    getProduct,
-    getSingleProduct,
-    listCompanies,
-    listUsers,
-    listProducts,
-    listOrders,
-    listUserOrders,
-    showUserProfile,
-    handleSignIn,
-    handleSignOut,
-    handleSignUp,
-    handleCheckout,
-    getProductBy_id,
-    getProductBy_idMW,
-    getOrderBy_id,
-    deleteOrderBy_id,
-    handlePurchase_t,
-  };
+  // filters the items by category into a new array
+  let filteredItems = items.filter((item) => {
+    if (item.category === category) {
+      return item;
+    }
+  });
 
+  return filteredItems;
+};
+
+module.exports = {
+  handleProduct,
+  handleSingleProduct,
+  sortCategory,
+  getProduct,
+  getSingleProduct,
+  listCompanies,
+  listUsers,
+  listProducts,
+  listOrders,
+  listUserOrders,
+  showUserProfile,
+  handleSignIn,
+  handleSignOut,
+  handleSignUp,
+  handleCheckout,
+  getProductBy_id,
+  getProductBy_idMW,
+  getOrderBy_id,
+  deleteOrderBy_id,
+  handlePurchase_t,
+};
